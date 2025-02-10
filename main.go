9215a274429c5db8925894082a8588576e1079ac
ElCabrii <gabriel.debure@ynov.com>
2025-02-10 14:18:16 +0100

    Add Dockerfile and docker-compose configuration; update main.go for new routing

--- main.go
+++ main.go
@@ -8,11 +8,27 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Route struct {
+	Endpoint string
+	Handler  func(http.ResponseWriter, *http.Request)
+}
+
+var Endpoints = []Route{
+	{Endpoint: "/" , Handler: IndexHandler},
+}
+
+
 func main() {
 	router := mux.NewRouter()
 	fmt.Println("Starting server...")
 	for _, route := range Endpoints {
 		router.HandleFunc(route.Endpoint, route.Handler).Methods("GET")
 	}
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
 }
